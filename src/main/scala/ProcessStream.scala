395105fa6a9c15c7e3e83fa7ccfed915921b85c7
Ubuntu <ubuntu@ip-172-31-1-108.us-west-2.compute.internal>
2016-10-02 20:43:13 +0000

    stream and batch update

--- src/main/scala/ProcessStream.scala
+++ src/main/scala/ProcessStream.scala
@@ -24,22 +24,20 @@ case class Record(word:String)
 
 def main(args: Array[String]){
 
-        val conf = new SparkConf()
+        val conf = new SparkConf(true)
 	.setAppName("stackoverflowstreamprocessing")
-	.setMaster("local[*]")
 	.set("spark.cassandra.connection.host","ip-172-31-1-106")
         .set("spark.cassandra.auth.username", "cassandra")
         .set("spark.cassandra.auth.password", "cassandra")
 
         val sc = new SparkContext(conf)
-	val ssc = new StreamingContext(sc, Seconds(10))
+	val ssc = new StreamingContext(sc, Seconds(1))
 	val sqlContext = new SQLContext(sc)
         ssc.checkpoint("checkpoint") 
-
 	import sqlContext.implicits._
 
-      val  topics= List("stackoverflow").toSet
-      val  kafkabrokers = Map("metadata.broker.list" -> "ec2-52-43-50-72.us-west-2.compute.amazonaws.com:9092, ec2-54-69-129-249.us-west-2.compute.amazonaws.com:9092, ec2-54-69-102-27.us-west-2.compute.amazonaws.com:9092, ec2-54-69-151-54.us-west-2.compute.amazonaws.com:9092")
+      val  topics= List("posts").toSet
+      val  kafkabrokers = Map("metadata.broker.list" -> "ec2-52-32-170-191.us-west-2.compute.amazonaws.com:9092, ec2-52-42-114-72.us-west-2.compute.amazonaws.com:9092, ec2-52-34-46-77.us-west-2.compute.amazonaws.com:9092")
      val directKafkaStream = KafkaUtils.createDirectStream[String,String,StringDecoder,StringDecoder](ssc,kafkabrokers,topics).map(_._2)
    
 	directKafkaStream.print()
@@ -48,31 +46,42 @@ sqlContext.jsonRDD(rdd).registerTempTable("realtimeposts")
       
 if (rdd.toLocalIterator.nonEmpty) {
 //        sqlContext.jsonRDD(rdd).registerTempTable("realtimeposts")
-val tagsofquestion = sqlContext.sql("Select title,tags,is_answered,link from realtimeposts")
+val tagsofquestion = sqlContext.sql("Select tags,link,title,is_answered from realtimeposts")
+tagsofquestion.show()
+tagsofquestion.printSchema()
 val tagquestion = tagsofquestion.select("title","tags","is_answered").withColumn("tag",explode($"tags"))
 tagquestion.registerTempTable("tagquestion")
-val tags = sqlContext.sql("select tags from realtimeposts").withColumn("tag",explode($"tags")).select("tag")
+tagquestion.show()
+val tags = sqlContext.sql("select tags from realtimeposts where is_answered!=true").withColumn("tag",explode($"tags")).select("tag")
 val tagcount = tags.map(x=>(x(0).toString,1)).reduceByKey(_+_).toDF()
 tagcount.show()
-val newtagcount =tagcount.select($"_1".alias("TagName"),$"_2".alias("Count"))
+val newtagcount =tagcount.select($"_1".alias("tagname"),$"_2".alias("count"))
 newtagcount.show()
 val trendingtagsfromdb = sqlContext.read.format("org.apache.spark.sql.cassandra").options(Map("table" -> "trendingtags", "keyspace" -> "stackoverflow")).load
 trendingtagsfromdb.show()
+val sortresult = trendingtagsfromdb.unionAll(newtagcount).map(x=>(x.getString(0),x.getInt(1))).reduceByKey(_+_).sortBy(_._2,false).take(10)
+val arraytordd = ssc.sparkContext.parallelize(sortresult).toDF()
+arraytordd.select($"_1".alias("tagname"),$"_2".alias("count")).write.format("org.apache.spark.sql.cassandra").options(Map("table" -> "trendingtags", "keyspace" -> "stackoverflow")).mode(SaveMode.Overwrite).save()
 
-val updatedtagcount = trendingtagsfromdb.unionAll(newtagcount)
-//.map(x=>(x(0).toString,x(1).toString)).toDF()
-//.reduceByKey(_+_).toDF()
-updatedtagcount.printSchema()
-updatedtagcount.show()
 
-sqlContext.sql( "SELECT tags,link,title,is_answered FROM realtimeposts").map(RealTimePosts(_)).saveToCassandra("stackoverflow","realtimeposts")
+//.saveToCassandra("stackoverflow","trendingtags")
+//takeOrdered(10)(Ordering[Int].reverse.on(y=>y._2))
+//val yz = ssc.parallelize(xyz)
+//yz.saveToCassandra("stackoverflow","trendingtags")
+
+//trendingtagsfromdb.unionAll(newtagcount).map(x=>(x.getString(0),x.getInt(1))).reduceByKey(_+_).saveToCassandra("stackoverflow","trendingtags")
+tagsofquestion.map(RealTimePosts(_)).saveToCassandra("stackoverflow","realtimeposts", SomeColumns("tags","link","title","answered"))
 //x.toDF().write.format("org.apache.spark.sql.cassandra").options(Map("table" -> "realtimeposts", "keyspace" -> "stackoverflow")).mode(SaveMode.Append).save()
 
 val df = sqlContext.read.format("org.apache.spark.sql.cassandra").options(Map("table" -> "tagtousers", "keyspace" -> "stackoverflow")).load
+df.show(20)
 val tagtosingleuser = df.withColumn("user",explode($"users"))
 tagtosingleuser.registerTempTable("tagtousers")
+
 val fin = sqlContext.sql("select user,title from tagquestion r Join tagtousers t on r.tag = t.tag where is_answered!=true")
+fin.show()
 val finremove = sqlContext.sql("select user,title from tagquestion r Join tagtousers t on r.tag = t.tag where is_answered =true")
+finremove.show()
 fin.map(x=>(x(0).toString,Set(x(1).toString))).reduceByKey(_ ++ _).saveToCassandra("stackoverflow","usertoquestion",SomeColumns("userid","questions" append)) 
 finremove.map(x=>(x(0).toString,Set(x(1).toString))).reduceByKey(_ ++ _).saveToCassandra("stackoverflow","usertoquestion",SomeColumns("userid","questions" remove))
 //val temp = rdd.map(_.split(" ",-1)).map(n=>RealTimePosts(n(0),n(1))).toDF()
--- src/main/scala/Program.scala
+++ src/main/scala/Program.scala
@@ -14,7 +14,7 @@ object Program{
 
 case class Post(id: String,PostTypeId: String,ParentId: String,AcceptedAnswerId: String,CreationDate: String,Score: String,ViewCount: String,owneruserid: String,LastEditorUserId: String,LastEditorDisplayName: String,LastEditDate: String,LastActivityDate: String,Tags: String,AnswerCount: String,CommentCount: String,FavoriteCount: String,CommunityOwnedDate: String,OwnerDisplayName: String)
 
-case class User(id: String,Reputation: String, Location: String, CreationDate: String, DisplayName: String, LastAccessDate: String, Views: String, UpVotes: String, DownVotes: String, AccountId: String, WebsiteUrl: String, Age: String)
+case class User(id: String,reputation: String, Location: String, CreationDate: String, displayname: String, LastAccessDate: String, Views: String, upvotes: String, downvotes: String, AccountId: String, WebsiteUrl: String, Age: String)
 
 case class Vote(id: String,PostId: String, VoteTypeId: String,userid: String, CreationDate: String, BountyAmount: String)
 
@@ -22,10 +22,10 @@ case class Expertise(UserId: String, TagName: String)
 
 case class Tag(UserId: String, domain:Array[String])
 
-case class TagCount(UserId:String, TagName:String, Total:Int)
-case class TrendingTags(tag:Array[String])
+//case class TagCount(UserId:String, TagName:String, Total:Int)
+//case class TrendingTags(tag:Array[String])
 
-case class FinalTags(tag: String, number: Int)
+//case class FinalTags(tag: String, number: Int)
 
 case class TagsFile(id:String, TagName:String,Count:Int, ExcerptPostId:String, WikiPostId:String)
 
@@ -33,17 +33,13 @@ def main(args: Array[String]){
 
 val conf = new SparkConf(true).set("spark.cassandra.connection.host","ip-172-31-1-106").set("spark.cassandra.auth.username", "cassandra").set("spark.cassandra.auth.password", "cassandra")
 
-val sc = new SparkContext("spark://ec2-52-43-50-72.us-west-2.compute.amazonaws.com:7077","stackoverflow",conf)
+val sc = new SparkContext("spark://ec2-52-24-206-90.us-west-2.compute.amazonaws.com:7077","stackoverflow",conf)
 val sqlContext = new org.apache.spark.sql.hive.HiveContext(sc)
 
 import sqlContext.implicits._
 
-val removetagsextralines = sc.textFile("hdfs://ec2-52-43-50-72.us-west-2.compute.amazonaws.com:9000/stackoverflow/tagsfileextra.txt")
-
-val tagsfilebeforeremovinglines = sc.textFile("hdfs://ec2-52-43-50-72.us-west-2.compute.amazonaws.com:9000/stackoverflow/Tags.xml")
-
-tagsfilebeforeremovinglines.count()
-
+val removetagsextralines = sc.textFile("hdfs://ec2-52-24-206-90.us-west-2.compute.amazonaws.com:9000/input/tagsfileextra.txt")
+val tagsfilebeforeremovinglines = sc.textFile("hdfs://ec2-52-24-206-90.us-west-2.compute.amazonaws.com:9000/input/Tags.xml")
 val tagsfileafterremovinglines = tagsfilebeforeremovinglines.subtract(removetagsextralines)
 
  val tagsfile = tagsfileafterremovinglines.map(r=>{val elements =scala.xml.XML.loadString(r)
@@ -52,15 +48,15 @@ id}).map(_.split(",",-1)).map(n=>TagsFile(n(0),n(1),n(2).toInt,n(3),n(4))).toDF(
 
 tagsfile.registerTempTable("tags")
 
-val tag_count = sqlContext.sql("SELECT TagName,Count from tags order by Count desc limit 10")
+val tag_count = sqlContext.sql("SELECT count,tagname from tags order by count desc limit 10")
 
+tag_count.printSchema()
+tag_count.show()
+//tag_count.rdd.saveToCassandra("stackoverflow","trendingtags",SomeColumns("tagname","count"))
 tag_count.write.format("org.apache.spark.sql.cassandra").options(Map("table" -> "trendingtags", "keyspace" -> "stackoverflow")).mode(SaveMode.Append).save()
-//tag_count.rdd.saveToCassandra("stackoverflow","trendingtags",SomeColumns("tagtitle","total"))
-
-val removepostsextralines = sc.textFile("hdfs://ec2-52-43-50-72.us-west-2.compute.amazonaws.com:9000/stackoverflow/postsfileextra.txt")
-
-val postsfilebeforeremovinglines = sc.textFile("hdfs://ec2-52-43-50-72.us-west-2.compute.amazonaws.com:9000/Input/Posts.xml")
-
+/**
+val removepostsextralines = sc.textFile("hdfs://ec2-52-24-206-90.us-west-2.compute.amazonaws.com:9000/input/postsfileextra.txt")
+val postsfilebeforeremovinglines = sc.textFile("hdfs://ec2-52-24-206-90.us-west-2.compute.amazonaws.com:9000/input/Posts.xml")
 val postsfileafterremovinglines = postsfilebeforeremovinglines.subtract(removepostsextralines)
 
 val post = postsfileafterremovinglines.map(r=>{
@@ -69,8 +65,6 @@ val id = (elements \ "@Id").text  +","+ (elements \ "@PostTypeId").text +","+ (e
 id
 }).map(_.split(",",-1)).map(p=>Post(p(0),p(1),p(2),p(3),p(4),p(5),p(6),p(7),p(8),p(9),p(10),p(11),p(12),p(13),p(14),p(15),p(16),p(17))).toDF()
 
-post.printSchema()
-post.show(10)
 post.registerTempTable("post")
 
 val toptags = sqlContext.sql("SELECT p.owneruserid,d.Tags from post p JOIN (SELECT * from post where AcceptedAnswerId is not null) d ON d.AcceptedAnswerId = p.id where p.CreationDate is not null and p.id is not null and p.owneruserid is not null  and months_between(current_timestamp(),p.CreationDate)<13 group by p.owneruserid,d.Tags,p.CreationDate").map(r=> {
@@ -81,7 +75,7 @@ val q = toptags.toDF("UserId","domain")
 
 val x = q.withColumn("TagName",explode($"domain"))
 
-x.printSchema()
+//x.printSchema()
 
 x.registerTempTable("Expertise")
 
@@ -96,19 +90,20 @@ val ranked = tagsofuser.withColumn("rank", dense_rank.over(overTotal))
 
 val finaltoptags = ranked.where('total>=10).where('rank <= 3)
 
-finaltoptags.persist()
+val tagtousertotag = finaltoptags.select(finaltoptags("UserId"),finaltoptags("TagName"))
 
-finaltoptags.select(finaltoptags("UserId"),finaltoptags("TagName")).map(x=>(x(1).toString,List(x(0).toString))).reduceByKey(_ ++ _).saveToCassandra("stackoverflow","tagtousers",SomeColumns("tag","users"))
+tagtousertotag.persist()
+tagtousertotag.map(x=>(x(0).toString,List(x(1).toString))).reduceByKey(_ ++ _).saveToCassandra("stackoverflow","toptags",SomeColumns("userid","tags"))
+tagtousertotag.map(x=>(x(1).toString,List(x(0).toString))).reduceByKey(_ ++ _).saveToCassandra("stackoverflow","tagtousers",SomeColumns("tag","users"))
 
-finaltoptags.select(finaltoptags("UserId"),finaltoptags("TagName")).map(x=>(x(0).toString,List(x(1).toString))).reduceByKey(_ ++ _).saveToCassandra("stackoverflow","toptags",SomeColumns("userid","tags"))
+//tagtousertotag.map(x=>(x(0).toString,List(x(1).toString))).reduceByKey(_ ++ _).saveToCassandra("stackoverflow","toptags",SomeColumns("userid","tags"))
 
 //finaltoptags.select(finaltoptags("UserId"),finaltoptags("TagName")).map(x=>(x(1).toString,List(x(0).toString))).reduceByKey(_ ++ _).saveToCassandra("stackoverflow","tagtousers",SomeColumns("tag","users"))
 
 //tagsofuser.write.format("org.apache.spark.sql.cassandra").options(Map("table" -> "tagsofuser", "keyspace" -> "stackoverflow")).mode(SaveMode.Append).save()
 
-val removeusersextralines = sc.textFile("hdfs://ec2-52-43-50-72.us-west-2.compute.amazonaws.com:9000/stackoverflow/usersfileextra.txt")
-val usersfilebeforeremovinglines = sc.textFile("hdfs://ec2-52-43-50-72.us-west-2.compute.amazonaws.com:9000/Input/Users.xml")
-usersfilebeforeremovinglines.count()
+val removeusersextralines = sc.textFile("hdfs://ec2-52-24-206-90.us-west-2.compute.amazonaws.com:9000/input/usersfileextra.txt")
+val usersfilebeforeremovinglines = sc.textFile("hdfs://ec2-52-24-206-90.us-west-2.compute.amazonaws.com:9000/input/Users.xml")
 val usersfileafterremovinglines = usersfilebeforeremovinglines.subtract(removeusersextralines)
 
 val user = usersfileafterremovinglines.map(r=>{
@@ -116,28 +111,15 @@ val elements =scala.xml.XML.loadString(r)
 val id = (elements \ "@Id").text +","+ (elements \ "@Reputation").text +","+ (elements \ "@Location").text.split(",")(0) +","+ (elements \ "@CreationDate").text +","+ (elements \ "@DisplayName").text +","+ (elements \ "@LastAccessDate").text +","+ (elements \ "@Views").text +","+ (elements \ "@UpVotes").text +","+ (elements \ "@DownVotes").text +","+ (elements \ "@AccountId").text +","+ (elements \ "@WebsiteUrl").text +","+ (elements \ "@Age").text
 id}).map(_.split(",",-1)).map(u=>User(u(0),u(1),u(2),u(3),u(4),u(5),u(6),u(7),u(8),u(9),u(10),u(11))).toDF()
 
-user.printSchema()
-user.show(5)
 user.registerTempTable("user")
 
-val userprofile = sqlContext.sql("Select id, DisplayName,DownVotes,Reputation, UpVotes from user")
-userprofile.printSchema()
-//userprofile.rdd.saveToCassandra("stackoverflow","userprofile",SomeColumns("Id","DisplayName","Reputation","UpVotes","DownVotes"))
+val userprofile = sqlContext.sql("Select id,displayname,downvotes,reputation,upvotes from user")
+//userprofile.rdd.saveToCassandra("stackoverflow","userprofile",SomeColumns("id","displayname","reputation","upvotes","downvotes"))
 userprofile.write.format("org.apache.spark.sql.cassandra").options(Map("table" -> "userprofile", "keyspace" -> "stackoverflow")).mode(SaveMode.Append).save()
-
-//val toptagsforuser = sqlContext.sql(" SELECT UserId, TagName, Count(*) Total from Expertise where UserId is NOT NULL group by UserId, TagName HAVING Total>=5 order by Total desc")
-
-//toptagsforuser.printSchema()
-
-//toptagsforuser.rdd.saveToCassandra("stackoverflow","toptagsforuser",SomeColumns("UserId","TagName","Total"))
-
-//toptagsforuser.write.format("org.apache.spark.sql.cassandra").options(Map("table" -> "toptagsforuser", "keyspace" -> "stackoverflow")).mode(SaveMode.Append).save()
-//toptagsforuser.rdd.saveAsTextFile("hdfs://ec2-52-43-50-72.us-west-2.compute.amazonaws.com:9000/toptagsforuser/")
-
-val removevotesextralines = sc.textFile("hdfs://ec2-52-43-50-72.us-west-2.compute.amazonaws.com:9000/stackoverflow/votesfileextra.txt")
-
-val votesfilebeforeremovinglines = sc.textFile("hdfs://ec2-52-43-50-72.us-west-2.compute.amazonaws.com:9000/Input/Votes.xml")
-
+*/
+/**
+val removevotesextralines = sc.textFile("hdfs://ec2-52-24-206-90.us-west-2.compute.amazonaws.com:9000/input/votesfileextra.txt")
+val votesfilebeforeremovinglines = sc.textFile("hdfs://ec2-52-24-206-90.us-west-2.compute.amazonaws.com:9000/input/Votes.xml")
 val votesfileafterremovinglines = votesfilebeforeremovinglines.subtract(removevotesextralines)
 
 val vote = votesfileafterremovinglines.map(r=>{
@@ -146,20 +128,15 @@ val id = (elements \ "@Id").text +","+ (elements \ "@PostId").text +","+ (elemen
 id}).map(_.split(",",-1)).map(v=>Vote(v(0),v(1),v(2),v(3),v(4),v(5))).toDF()
 
 vote.registerTempTable("vote")
-
-
-val favoritevotes = sqlContext.sql("SELECT owneruserid, p.id, Count(*) numberofusers from post p JOIN vote v on v.PostId = p.id where v.VoteTypeId='5' group by owneruserid,p.id") 
-favoritevotes.show(20)
+*/
+//val favoritevotes = sqlContext.sql("SELECT owneruserid, p.id, Count(*) numberofusers from post p JOIN vote v on v.PostId = p.id where v.VoteTypeId='5' group by owneruserid,p.id") 
 //favoritevotes.write.format("org.apache.spark.sql.cassandra").options(Map("table" -> "favoritevotes", "keyspace" -> "stackoverflow")).mode(SaveMode.Append).save()
-//favoritevotes.rdd.saveAsTextFile("hdfs://ec2-52-43-50-72.us-west-2.compute.amazonaws.com:9000/favoritevotes/")
 
-val unansweredquestionswithbounty = sqlContext.sql("Select count(*) from vote where VoteTypeId='8'")
+//val unansweredquestionswithbounty = sqlContext.sql("Select count(*) from vote where VoteTypeId='8'")
 
-val unansweredcountfortag = sqlContext.sql("Select p.id,p.Tags from post Join vote on p.id = v.PostId where v.VoteTypeId='8' and p.PostTypeId='1' and (p.AcceptedAnswerId=null or p.AcceptedAnswerId= ' ')") 
-unansweredcountfortag.printSchema()
-unansweredcountfortag.collect()
+//val unansweredcountfortag = sqlContext.sql("Select p.id,p.Tags from post Join vote on p.id = v.PostId where v.VoteTypeId='8' and p.PostTypeId='1' and (p.AcceptedAnswerId=null or p.AcceptedAnswerId= ' ')") 
 
-unansweredquestionswithbounty.write.format("org.apache.spark.sql.cassandra").options(Map("table" -> "questionscountwithbounty", "keyspace" -> "stackoverflow")).mode(SaveMode.Append).save()
+//unansweredquestionswithbounty.write.format("org.apache.spark.sql.cassandra").options(Map("table" -> "questionscountwithbounty", "keyspace" -> "stackoverflow")).mode(SaveMode.Append).save()
 
 }
 }
