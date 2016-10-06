c31605df71d71ad4035d934a2b0c58a7e4da27c9
Ubuntu <ubuntu@ip-172-31-1-109.us-west-2.compute.internal>
2016-10-06 07:51:41 +0000

    Web UI and Kafka updates

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,11 +1,11 @@
-# jsonify creates a json representation of the response
+#jsonify creates a json representation of the response
 from flask import jsonify
 from flask import Flask, render_template, request
 from app import app
 from flask import jsonify
 from cassandra.cluster import Cluster
 
-cluster = Cluster(['ec2-52-43-50-72.us-west-2.compute.amazonaws.com'])
+cluster = Cluster(['ec2-52-39-76-176.us-west-2.compute.amazonaws.com'])
 session = cluster.connect('stackoverflow')
 
 @app.route('/')
@@ -16,26 +16,56 @@ def user():
 def result():
    if request.method == 'POST':
       userid = request.form['userid']
-      stmt1 = " Select * from userprofile where id ='"+ str(userid) + "' ;"
-      stmt2 = "Select tags from toptags where userid ='"+ str(userid) + "' ;"	
-      stmt3 = "Select questions from usertoquestion where userid ='"+ str(userid) + "' ;"
-      stmt4 = "Select * from trendingtags"
+      output = execute(userid)
+      return render_template("dashboard.html",json_response=output)
+
+@app.route('/dashboard/<userid>',methods = ['GET'])
+def getdata(userid):
+      output = execute(userid)
+      return jsonify(output)
+
+def execute(userid):
+      stmt1 = " Select * from userprofile where id ='"+ str(userid) + "';"
+      stmt2 = "Select tags from toptags where userid ='"+ str(userid)+ "';"
+      stmt3 = "Select questions from usertoquestion where userid ='"+ str(userid)+ "';"
+      stmt4 = "Select tagname,count from trendingtags limit 10"
+      stmt5 = "Select upvotes from realtimeupvotes where id ='"+ str(userid) + "';"
+      stmt6 = "Select downvotes from realtimedownvotes where id ='"+ str(userid) + "';"
       response1 = session.execute(stmt1)
       response2 = session.execute(stmt2)
       response3 = session.execute(stmt3)
       response4 = session.execute(stmt4)
+      response5 = session.execute(stmt5)
+      response6 = session.execute(stmt6)
       userprofile = []
       toptags = []
       trendingtags = []
       questions = []
+      upvotes = []
+      downvotes = []
       for val in response1:
          userprofile.append(val)
       for val in response2:
-	 toptags.append(val.tags)
+         toptags.append(val.tags)
       for val in response3:
-	 questions.append(val.questions)
+         questions.append(val.questions)
       for val in response4:
-	 trendingtags.append(val)
-      json_response = [{"DisplayName": user.DisplayName, "Reputation": user.Reputation, "UpVotes": user.UpVotes,"DownVotes":user.DownVotes} for user in userprofile]
-      json_response1 = [{"tagname": t.TagName,"count":t.Count} for t in trendingtags]
-      return render_template("dashboard.html",user = json_response,trendingtags= json_response1,toptags=toptags,questions=questions)
+         trendingtags.append(val)
+      for val in response5:
+	 upvotes.append(val)
+      for val in response6:
+	 downvotes.append(val)	
+      top3tags = []
+      questionslist = []
+      i=0
+      while (toptags and i< len(toptags[0])):
+         top3tags.append(str(toptags[0][i]))
+         i += 1
+      i=0
+      while(questions and i<len(questions[0])):
+  	 questionslist.append(str(questions[0][i]))
+         i+=1	
+      userstats = [{"id": str(user.id), "displayname": str(user.displayname), "reputation": str(user.reputation), "upvotes": str(user.upvotes),"downvotes":str(user.downvotes)} for user in userprofile]
+      tagcount = [{"tagname": str(t.tagname),"count":t.count} for t in trendingtags]
+      return [userstats,top3tags,questionslist,tagcount,upvotes,downvotes]
+      
--- kafka/producer.py
+++ kafka/producer.py
@@ -6,15 +6,25 @@ import threading, logging, time
 
 def main(): 
    producer = KafkaProducer(bootstrap_servers='localhost:9092')
-   jsonFile = open("realdata.json", 'r')
+   jsonFile = open("posts.json", 'r')
    count = 0
- 
+
   # while True:
+   time.sleep(20)
    for line in jsonFile:
-      producer.send('stackoverflow', line)
-      count = count + 1 
-      print ("sent New Post"+line)
-      if count>30:
+      if "upvotes" in line:
+	  producer.send('votes',line)
+	  print ("sent New upVotes"+line)
+      elif "downvotes" in line:
+	  producer.send('downvotes',line)
+	  print("sent new downvotes"+line)
+      else:	
+          producer.send('posts', line)
+	  print ("sent New Post"+line)
+      count = count + 1
+      if(count%10 == 0):	 
+         time.sleep(3)
+      if count>75:
          break
 
 if __name__ == "__main__":
